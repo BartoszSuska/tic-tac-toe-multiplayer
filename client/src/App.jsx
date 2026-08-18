165f5b70c222f9eb5ab004d137a2bfb4789b616d
Bartosz Suska <60578620+BartoszSuska@users.noreply.github.com>
2026-08-18 15:14:27 +0200

    authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,28 @@ import { useState } from 'react'
 import './App.css'
 import SignUp from './components/SignUp.jsx'
 import Login from './components/Login.jsx'
+import {StreamChat} from 'stream-chat'
+import Cookies from 'universal-cookie'
 
 function App() {
+  const cookies = new Cookies()
+  const api_key = "ha9g99wf5pna"
+  const token = cookies.get("token")
+  const client = StreamChat.getInstance(api_key)
+
+  if(token){
+    client.connectUser({
+      id: cookies.get("userId"),
+      name: cookies.get("username"),
+      firstName: cookies.get("firstName"),
+      lastName: cookies.get("lastName"),
+      hashedPassword: cookies.get("hashedPassword")
+    }, token
+    ).then((user) => {
+      console.log(user)
+    })
+  }
+
   return <div className="App">
     <SignUp />
     <Login />
